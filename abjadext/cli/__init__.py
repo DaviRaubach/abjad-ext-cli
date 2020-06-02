af4ca2f9eb54de4415528d0bee93995e5ec80a1a
Trevor Bača <trevor.baca@gmail.com>
2020-06-02 13:32:12 -0400

    Cleaned up package initializer.

--- abjadext/cli/__init__.py
+++ abjadext/cli/__init__.py
@@ -1,9 +1,19 @@
 """
-Extension for managing score projects on the commandline.
+Extension for managing score projects at the commandline.
 """
-from .ManageBuildTargetScript import ManageBuildTargetScript  # noqa
-from .ManageMaterialScript import ManageMaterialScript  # noqa
-from .ManageScoreScript import ManageScoreScript  # noqa
-from .ManageSegmentScript import ManageSegmentScript  # noqa
-from .ScorePackageScript import ScorePackageScript  # noqa
-from ._version import __version__, __version_info__  # noqa
+from .ManageBuildTargetScript import ManageBuildTargetScript
+from .ManageMaterialScript import ManageMaterialScript
+from .ManageScoreScript import ManageScoreScript
+from .ManageSegmentScript import ManageSegmentScript
+from .ScorePackageScript import ScorePackageScript
+from ._version import __version__, __version_info__
+
+__all__ = [
+    "ManageBuildTargetScript",
+    "ManageMaterialScript",
+    "ManageScoreScript",
+    "ManageSegmentScript",
+    "ScorePackageScript",
+    "__version__",
+    "__version_info__",
+]
